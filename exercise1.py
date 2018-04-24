9f3b3e7d1f8a4141f1b5ef4a6b1a20172d9a298f
pythonwins <38597686+pythonwins@users.noreply.github.com>
2018-04-24 13:36:37 -0400

    Add files via upload
    User Input Exercise

---
+++ exercise1.py
@@ -0,0 +1,10 @@
+def when_hundred():
+    name = input("Please enter your name: ")
+    age = int(input("Please enter your age: "))
+    x = 100 - age
+    year = x + 2017
+    print("Hello, %s, in %s you will be 100 years old." % (name, year))
+    
+when_hundred()
+    
+    
